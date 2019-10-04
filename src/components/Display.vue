9296ff5b906b1eca471338b00633ef6b38a6e5b1
Riza Khan <khanriza@gmail.com>
2019-10-03 21:52:49 -0400

    initial commit

---
+++ src/components/Display.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 
+        v-for="quote in quoteArray"
+        @click="removeQuote($event)"
+        >{{quote}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quoteArray'],
+    methods: {
+    removeQuote(e) {
+       
+        this.quoteArray.splice(this.quoteArray.indexOf(e.target.innerHTML, 0), 1)
+        
+    }
+  }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="counter-container">
+        <Counter
+        :quoteArray='quoteArray' 
+        v-if="quoteArray"        
+                />
+      </div>
+      <textarea 
+      v-model="newQuote"
+      />
+
+    <button
+      @click="addQuote(newQuote)"
+    >Add Quote</button>
+
+    <Display :quoteArray="quoteArray"/>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+import Display from './Display.vue'
+
+export default {
+  name: 'Main',
+  data() {
+    return{
+      newQuote: '',
+      quoteArray: [],
+      
+    }
+  },
+  computed: {
+    
+  },
+    components: {
+      Counter,
+      Display,
+  },
+  methods: {
+    addQuote(data) {
+      if(data == '') {
+        alert('Please enter a quote')
+      } else {
+        if(this.quoteArray.length == 10) {
+          alert('Delete a quote to add another')
+        } else {
+            this.quoteArray.push(data)
+     this.counter = this.quoteArray.length
+     this.newQuote = '' 
+     this.newQuote.focus()
+        }
+
+     }
+      
+    },
+  }}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.counter-container {
+  border: solid 1px red;
+  height: 200px;
+}
+
+</style>
